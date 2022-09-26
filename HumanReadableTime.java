3c9002701d01fe0dd44062d93c7ad2851fbdbe96
unknown <jkub.sliwinski@gmail.com>
2022-09-26 14:46:20 +0200

    code wars

--- HumanReadableTime.java
+++ HumanReadableTime.java
@@ -6,7 +6,7 @@ public class HumanReadableTime {
     public static String makeReadable ( int seconds){
         int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
-        int timeInSeconds = seconds - hours * 3600 - minutes * 60;
+        int timeInSeconds = ((seconds % 3600) % 60);
 
 
         return (hours < 10 ? "0" + hours : hours)
